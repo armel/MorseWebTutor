6e4b313307aebcf2616f7716b5f93ec49a42ce5e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-12-06 13:15:46 +0100

    Fix bug

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -1,6 +1,6 @@
 function randomLetters(length) {
    var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+   var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';   
    var charactersLength = characters.length;
    for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -8,48 +8,125 @@ function randomLetters(length) {
    return result;
 }
 
-$(function(){
+function morseEncode(pattern, context, dot) {
+  letters = [ "a","b","c","d","e","f",
+              "g","h","i","j","k","l",
+              "m","n","o","p","q","r",
+              "s","t","u","v","w","x",
+              "y","z","0","1","2","3",
+              "4","5","6","7","8","9",
+              " "];
 
-  if ('speechSynthesis' in window) {
-    speechSynthesis.onvoiceschanged = function() {
-      var $voicelist = $('#voices');
+  symbols = [ ".-","-...","-.-.","-..",".","..-.",
+              "--.","....","..",".---","-.-",".-..",
+              "--","-.","---",".--.","--.-",".-.",
+              "...","-","..-","...-",".--","-..-",
+              "-.--","--..","-----",".----","..---","...--",
+              "....-",".....","-....","--...","---..","----.",
+              " "];
 
-      if($voicelist.find('option').length == 0) {
-        speechSynthesis.getVoices().forEach(function(voice, index) {
-          var $option = $('<option>')
-          .val(index)
-          .html(voice.name + (voice.default ? ' (default)' :''));
+  pattern = pattern.split('').join(' ');
+  console.log(pattern);
 
-          $voicelist.append($option);
-        });
+  var t = context.currentTime;
 
-        $voicelist.material_select();
+  var oscillator = context.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = localStorage.getItem('tone');
+
+  var gainNode = context.createGain();
+  gainNode.gain.setValueAtTime(0, t);
+
+  for (const c of pattern) {
+    for (const letter of symbols[letters.indexOf(c)]) {
+      switch(letter) {
+          case ".":
+              gainNode.gain.setValueAtTime(1, t);
+              t += dot;
+              gainNode.gain.setValueAtTime(0, t);
+              t += dot;
+              break;
+          case "-":
+              gainNode.gain.setValueAtTime(1, t);
+              t += 3 * dot;
+              gainNode.gain.setValueAtTime(0, t);
+              t += dot;
+              break;
+          case " ":
+              t += 7 * dot;
+              break;
       }
     }
+  }
 
-    $('#speak').click(function(){
-      var msg = new SpeechSynthesisUtterance();
-      var voices = window.speechSynthesis.getVoices();
-      msg.voice = voices[$('#voices').val()];
-      msg.rate = $('#rate').val() / 10;
-      msg.pitch = $('#pitch').val();
-      msg.tone = $('#tone').val();
-      msg.wpm = $('#wpm').val();
-      msg.letters = $('#letters').val();
-      msg.text = randomLetters(msg.letters)
-      console.log(msg.text);
+  oscillator.connect(gainNode);
+  gainNode.connect(context.destination);
 
-      for (const c of msg.text) {
-        console.log(c)
-        speechSynthesis.speak(c);
-      }
+  oscillator.start();
 
+  return t;
+}
 
-      msg.onend = function(e) {
-        console.log('Finished in ' + event.elapsedTime + ' seconds.');
-      };
-    })
+function morseSpeech(pattern, result) {
+  for (const c of pattern) {
+    var speech = new SpeechSynthesisUtterance(c);
+    
+    speech.lang = 'fr-CA';
+    speech.name = 'Amelie';
+    speech.rate = 1;
+    speech.pitch = 1;
+    
+    console.log(speech);
+
+    localStorage.setItem('tone',    $('#tone').val());
+    localStorage.setItem('wpm',     $('#wpm').val());
+    localStorage.setItem('letters', $('#letters').val());
+
+    $('#pattern').val(pattern);
+
+    speechSynthesis.speak(speech);
+  }
+}
+
+function generateMorse() { 
+  if ('speechSynthesis' in window) {
+      var AudioContext = window.AudioContext || window.webkitAudioContext;
+      var context = new AudioContext();
+      var dot = 1.2 / 15;
+
+      var tone = $('#tone').val();
+      var wpm = $('#wpm').val();
+      var letters = $('#letters').val();
+      var series = $('#series').val();
+
+      var pattern = randomLetters(letters);
+      tempo = morseEncode(pattern, context, dot);
+      setTimeout(function() {
+          morseSpeech(pattern);
+        }, tempo * 2000
+      );
   } else {
     $('#modal1').openModal();
   }
+}
+
+$(function() {
+  // Initialise value
+  if (localStorage.getItem('tone') !== null) {
+    $('#tone').val(localStorage.getItem('tone'));
+  }
+
+  if (localStorage.getItem('wpm') !== null) {
+    $('#wpm').val(localStorage.getItem('wpm') * 10);
+  }
+
+  if (localStorage.getItem('letters') !== null) {
+    $('#letters').val(localStorage.getItem('letters'));
+  }
+
+  $('#speak').click(function(){
+    var inter = setInterval(function() {
+        generateMorse(false);
+    }, 10000);
+  });
 });
